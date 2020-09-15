ff15e738dc36aa6096c5ea8c3ecd556e51586430
Henning Dieterichs <henning.dieterichs@live.de>
2020-09-15 10:52:30 +0200

    Updates program.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
 ﻿using HidLibrary;
 using System;
+using System.Linq;
 using System.Threading;
 
 namespace ConsoleAppUsbTest
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                foreach (var k in K380Keyboard.GetConnected())
+                {
+                    try
+                    {
+                        k.SetFunctionKeys(true);
+                        Console.WriteLine($"Could enable function keys for {k.Device.DevicePath}");
+                    }
+                    catch (Exception e)
+                    {
+                        //Console.WriteLine("Error " + e);
+                    }
+                }
+
+                Thread.Sleep(10 * 1000);
+            }
+        }
+    }
+
+    class K380Keyboard
+    {
         static int HID_VENDOR_ID_LOGITECH = 0x046d;
         static int HID_DEVICE_ID_K380 = 0xb342;
-
 
         static byte[] k380_seq_fkeys_on = { 0x10, 0xff, 0x0b, 0x1e, 0x00, 0x00, 0x00 };
         static byte[] k380_seq_fkeys_off = { 0x10, 0xff, 0x0b, 0x1e, 0x01, 0x00, 0x00 };
@@ -22,42 +46,38 @@ namespace ConsoleAppUsbTest
             bool r;
 
             if (enable)
+            {
                 r = device.Write(k380_seq_fkeys_on);
+            }
             else
+            {
                 r = device.Write(k380_seq_fkeys_off);
+            }
 
             device.CloseDevice();
 
             if (!r)
+            {
                 throw new Exception("Could not active function keys");
+            }
         }
 
-        static void Main(string[] args)
+        public static K380Keyboard[] GetConnected()
         {
-            var devicePath = "\\\\?\\hid#{00001124-0000-1000-8000-00805f9b34fb}_vid&0002046d_pid&b342&col06#9&3af9a3fe&0&0005#{4d1e55b2-f16f-11cf-88cb-001111000030}";
+            var devices = HidDevices.Enumerate(HID_VENDOR_ID_LOGITECH, HID_DEVICE_ID_K380).Where(d => d.IsConnected).Select(d => new K380Keyboard(d)).ToArray();
+            return devices;
+        }
 
-            //var dev = HidDevices.Enumerate(HID_VENDOR_ID_LOGITECH, HID_DEVICE_ID_K380).First(d => d.DevicePath == devicePath);
-            //SetFunctionKeys(dev, true);
+        public HidDevice Device { get; }
 
-            var connected = false;
+        public K380Keyboard(HidDevice device)
+        {
+            Device = device;
+        }
 
-            while (true)
-            {
-                Thread.Sleep(10 * 1000);
-                if (connected != HidDevices.IsConnected(devicePath))
-                {
-                    connected = !connected;
-                    Console.WriteLine("Changed to " + (connected ? "connected" : "disconnected"));
-
-                    if (connected)
-                    {
-                        var dev = HidDevices.GetDevice(devicePath);
-                        SetFunctionKeys(dev, true);
-                    }
-                }
-            }
-
-            Console.ReadLine();
+        public void SetFunctionKeys(bool enable)
+        {
+            SetFunctionKeys(Device, enable);
         }
     }
 }
